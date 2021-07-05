ad4a22b7b6fac7ba4f6f9831d080c2aec873a629
mwladyka126 <71927655+mwladyka126@users.noreply.github.com>
2021-07-05 18:39:57 +0200

    add Navbar Component to make header responsive

---
+++ src/components/features/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+import clsx from "clsx";
+import styles from "./Navbar.module.scss";
+
+const Component = ({ className, children }) => (
+  <ul className={clsx(className, styles.navList)}>
+    <li>
+      <Link to={"/projects/"} className={styles.link}>
+        <span>.</span>projects<span>( )</span>
+      </Link>
+    </li>
+    <li>
+      <Link to={"/about/"} className={styles.link}>
+        <span>.</span>about<span>( 'me')</span>
+      </Link>
+    </li>
+    <li>
+      <a
+        href="mailto:mwladyka126@gmail.com"
+        target="_blanket"
+        className={styles.link}
+      >
+        <span>.</span>contact<span>( )</span>
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://github.com/mwladyka126"
+        target="_blanket"
+        className={styles.icon}
+      >
+        {" "}
+        <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.linkedin.com/in/malgorzata-wladyka/"
+        target="_blanket"
+        className={styles.icon}
+      >
+        {" "}
+        <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+      </a>
+    </li>
+  </ul>
+);
+
+Component.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export { Component as Navbar, Component as NavbarComponent };
--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Navbar } from "./../../features/Navbar/Navbar";
 
 import clsx from "clsx";
 
@@ -12,6 +13,7 @@ import styles from "./Header.module.scss";
 const Component = ({ className, children }) => {
   const [menu, setMenu] = useState(false);
   const showMenu = () => setMenu(!menu);
+
   return (
     <div className={clsx(className, styles.root)}>
       <div className={styles.container}>
@@ -20,49 +22,7 @@ const Component = ({ className, children }) => {
             mw
           </Link>
         </div>
-        <div className={styles.navigation + " " + styles.menuBig}>
-          <ul className={styles.navigation_list}>
-            <li>
-              <Link to={"/projects/"} className={styles.link}>
-                .projects( )
-              </Link>
-            </li>
-            <li>
-              <Link to={"/about/"} className={styles.link}>
-                .about('me')
-              </Link>
-            </li>
-            <li>
-              <a
-                href="mailto:mwladyka126@gmail.com"
-                target="_blanket"
-                className={styles.link}
-              >
-                .contact( )
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/mwladyka126"
-                target="_blanket"
-                className={styles.icon}
-              >
-                {" "}
-                <FontAwesomeIcon icon={faGithub} className={styles.icon} />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/malgorzata-wladyka/"
-                target="_blanket"
-                className={styles.icon}
-              >
-                {" "}
-                <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Navbar className={styles.menuBig} />
         <div className={styles.hamburger}>
           <FontAwesomeIcon
             onClick={showMenu}
@@ -73,49 +33,7 @@ const Component = ({ className, children }) => {
           </FontAwesomeIcon>
         </div>
       </div>
-      {menu ? (
-        <ul className={styles.menuSmall}>
-          <li>
-            <Link to={"/projects/"} className={styles.link}>
-              .projects( )
-            </Link>
-          </li>
-          <li>
-            <Link to={"/about/"} className={styles.link}>
-              .about('me')
-            </Link>
-          </li>
-          <li>
-            <a
-              href="mailto:mwladyka126@gmail.com"
-              target="_blanket"
-              className={styles.link}
-            >
-              .contact( )
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/mwladyka126"
-              target="_blanket"
-              className={styles.icon}
-            >
-              {" "}
-              <FontAwesomeIcon icon={faGithub} className={styles.icon} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/malgorzata-wladyka/"
-              target="_blanket"
-              className={styles.icon}
-            >
-              {" "}
-              <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
-            </a>
-          </li>
-        </ul>
-      ) : null}
+      {menu ? <Navbar className={styles.menuSmall} /> : null}
     </div>
   );
 };
