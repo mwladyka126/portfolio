7c5c035382c952c1bd5783a5bdff708eb2beddf3
mwladyka126 <71927655+mwladyka126@users.noreply.github.com>
2021-07-11 18:03:30 +0200

    another try

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-//const router = jsonServer.router("dist/db/app.json");
+
 const middlewares = jsonServer.defaults({
   static: "dist",
   noCors: true,
@@ -10,6 +10,5 @@ const middlewares = jsonServer.defaults({
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
-//server.use(router);
 
 server.listen(port);
